0864250f02694379601eda7e9942dc4c36b3ed17
waleedd189 <waleedd2004@gmail.com>
2026-02-17 22:41:33 +0200

    Update index.js

--- api/index.js
+++ api/index.js
@@ -4,20 +4,15 @@ const path = require('path');
 module.exports = async (req, res) => {
 res.setHeader('Access-Control-Allow-Origin', '*');
 res.setHeader('Content-Type', 'application/json');
+
 const { surah = 1 } = req.query;
+
 try {
 const filePath = path.join(process.cwd(), 'quran_data.json');
 const jsonData = fs.readFileSync(filePath, 'utf-8');
-const quran = JSON.parse(jsonData);
-if (quran[surah]) {
-res.status(200).json({
-creator: "Waleed The Source",
-quran_data: quran[surah]
-});
-} else {
-res.status(404).json({ error: "السورة مش عندي يا برنس" });
-}
+const allAyas = JSON.parse(jsonData);
+
 } catch (e) {
-res.status(500).json({ error: "فيه مشكلة في السيرفر" });
+res.status(500).json({ error: "خطأ في السيرفر" });
 }
 };

22cd5363275c6c3a23524d24742e42b67f0c00ac
waleedd189 <waleedd2004@gmail.com>
2026-02-17 22:04:54 +0200

    Set Content-Type header for JSON response

--- api/index.js
+++ api/index.js
@@ -1,5 +1,6 @@
 module.exports = async (req, res) => {
 res.setHeader('Access-Control-Allow-Origin', '*');
+res.setHeader('Content-Type', 'application/json');
 const { surah = 1 } = req.query;
 const resp = await fetch('https://api.alquran.cloud/v1/surah/' + surah);
 const info = await resp.json();

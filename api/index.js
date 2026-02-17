e5d6622c30a271e05b8d50dca07f1753c0f9f097
waleedd189 <waleedd2004@gmail.com>
2026-02-17 22:52:56 +0200

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,18 +1,20 @@
-const fs = require('fs');
-const path = require('path');
-
 module.exports = async (req, res) => {
 res.setHeader('Access-Control-Allow-Origin', '*');
 res.setHeader('Content-Type', 'application/json');
-
 const { surah = 1 } = req.query;
-
 try {
-const filePath = path.join(process.cwd(), 'quran_data.json');
-const jsonData = fs.readFileSync(filePath, 'utf-8');
-const allAyas = JSON.parse(jsonData);
-
+const response = await fetch(https://api.alquran.cloud/v1/surah/${surah});
+const data = await response.json();
+if (data.code === 200) {
+res.status(200).json({
+creator: "Waleed Server",
+name: data.data.name,
+ayahs: data.data.ayahs
+});
+} else {
+res.status(404).json({ error: "السورة غير موجودة" });
+}
 } catch (e) {
-res.status(500).json({ error: "تأكد من رفع ملف quran_data.json كاملاً" });
+res.status(500).json({ error: "مشكلة في السيرفر" });
 }
 };

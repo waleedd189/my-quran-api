91ef1196bfb57452e6ed3d6f65f82513fd978323
waleedd189 <waleedd2004@gmail.com>
2026-02-17 22:46:03 +0200

    Update index.js

--- api/index.js
+++ api/index.js
@@ -4,15 +4,13 @@ const path = require('path');
 module.exports = async (req, res) => {
 res.setHeader('Access-Control-Allow-Origin', '*');
 res.setHeader('Content-Type', 'application/json');
-
 const { surah = 1 } = req.query;
-
 try {
 const filePath = path.join(process.cwd(), 'quran_data.json');
 const jsonData = fs.readFileSync(filePath, 'utf-8');
 const allAyas = JSON.parse(jsonData);
 
 } catch (e) {
-res.status(500).json({ error: "خطأ في السيرفر" });
+res.status(500).json({ error: "تأكد من رفع ملف quran_data.json كاملاً" });
 }
 };

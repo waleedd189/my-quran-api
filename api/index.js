b6d3378d122b3e947e8e6b9a5e404fc768768f67
waleedd189 <waleedd2004@gmail.com>
2026-02-17 22:21:29 +0200

    Refactor API to read Quran data from local file

--- api/index.js
+++ api/index.js
@@ -1,11 +1,23 @@
+const fs = require('fs');
+const path = require('path');
+
 module.exports = async (req, res) => {
 res.setHeader('Access-Control-Allow-Origin', '*');
 res.setHeader('Content-Type', 'application/json');
 const { surah = 1 } = req.query;
-const resp = await fetch('https://api.alquran.cloud/v1/surah/' + surah);
-const info = await resp.json();
+try {
+const filePath = path.join(process.cwd(), 'quran_data.json');
+const jsonData = fs.readFileSync(filePath, 'utf-8');
+const quran = JSON.parse(jsonData);
+if (quran[surah]) {
 res.status(200).json({
-creator: "Waleed",
-quran_data: info.data
+creator: "Waleed The Source",
+quran_data: quran[surah]
 });
+} else {
+res.status(404).json({ error: "السورة مش عندي يا برنس" });
+}
+} catch (e) {
+res.status(500).json({ error: "فيه مشكلة في السيرفر" });
+}
 };
